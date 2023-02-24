19ee4bd6793d0b0d6936a26319d5a394b299b169
Simone Viglione <zimoviglio51@gmail.com>
2023-02-24 16:21:23 +0100

    bonus finito, credo

--- js/main.js
+++ js/main.js
@@ -14,6 +14,33 @@ for (let i = 1; i <= 100; i++ ) {
 }
 
 
+//Bonus
+
+const containerDom = document.querySelector('.container')
+
+for (let i = 1; i <= 100; i++ ) {
+    const box = document.createElement('div')
+    box.classList.add('square');
+    containerDom.append(box);
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        box.classList.add('pasta-e-fagioli');
+        box.append('Pasta e Fagioli');
+    }
+    else if (i % 5 === 0) {
+        box.classList.add('fagioli');
+        box.append('Fagioli');
+    }
+    else if (i % 3 === 0) {
+        box.classList.add('pasta');
+        box.append('Pasta');
+    }
+    else {
+        box.classList.add('cifra');
+        box.append(i);
+    }
+}
+
 
 
 
